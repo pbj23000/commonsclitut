274965fb91e2e31de8d512d2f8091973f422ef10
CJ Miller <Christopher.Miller@Pearson.COM>
2015-06-09 10:02:07 -0600

    first code commit

---
+++ commonsclitut/src/main/java/cj/mf/commonsclitut/App.java
@@ -0,0 +1,13 @@
+package cj.mf.commonsclitut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
